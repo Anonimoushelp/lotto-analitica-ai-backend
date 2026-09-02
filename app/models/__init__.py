4d6d880eb7c07ca0278ce63b03862582d5ce10e3
helpcolombia2026 <abritovi@gmail.com>
2026-09-02 13:45:12 -0500

    feat(auth): register user model for metadata

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.lottery import Lottery
 from app.models.lottery_draw import LotteryDraw
+from app.models.user import User
 
-__all__ = ["Lottery", "LotteryDraw"]
+__all__ = ["Lottery", "LotteryDraw", "User"]
