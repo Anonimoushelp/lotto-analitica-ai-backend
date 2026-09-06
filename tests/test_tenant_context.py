15ae1889e6ed05cfffb3567771e55280ed358a41
Anonimous <abritovi@gmail.com>
2026-09-06 17:26:21 -0500

    fix: order TenantContext test imports

--- tests/test_tenant_context.py
+++ tests/test_tenant_context.py
@@ -1,5 +1,5 @@
-from fastapi import HTTPException
 import pytest
+from fastapi import HTTPException
 from sqlalchemy import create_engine, delete
 from sqlalchemy.orm import Session
 from sqlalchemy.pool import StaticPool
