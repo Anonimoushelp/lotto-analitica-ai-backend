3da817385b34a4b4450f80a2660495606894cb23
Anonimous <abritovi@gmail.com>
2026-09-08 16:31:04 -0500

    feat(saas): register plan models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,7 +2,18 @@ from app.models.audit_event import AuditEvent
 from app.models.lottery import Lottery
 from app.models.lottery_draw import LotteryDraw
 from app.models.membership import Membership
+from app.models.plan import Plan
+from app.models.plan_quota import PlanQuota
 from app.models.tenant import Tenant
 from app.models.user import User
 
-__all__ = ["AuditEvent", "Lottery", "LotteryDraw", "Membership", "Tenant", "User"]
+__all__ = [
+    "AuditEvent",
+    "Lottery",
+    "LotteryDraw",
+    "Membership",
+    "Plan",
+    "PlanQuota",
+    "Tenant",
+    "User",
+]
