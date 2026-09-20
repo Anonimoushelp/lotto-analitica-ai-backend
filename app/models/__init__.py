7f05818aae8d1ea141d37189e5b7ebdf1f761ebc
Anonimous <abritovi@gmail.com>
2026-09-20 07:02:07 -0500

    refactor: generalize lottery domain models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
+from app.models.draw_result import DrawResult
 from app.models.lottery import Lottery
 from app.models.lottery_draw import LotteryDraw
+from app.models.lottery_rule import LotteryRule
 from app.models.user import User
 
-__all__ = ["Lottery", "LotteryDraw", "User"]
+__all__ = ["DrawResult", "Lottery", "LotteryDraw", "LotteryRule", "User"]
