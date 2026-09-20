13f94def23b51d0f9b7a5cdef3f36299efa913bf
Anonimous <abritovi@gmail.com>
2026-09-20 07:15:45 -0500

    test: align draw integrity checks with multi-draw dates

--- tests/test_database_integrity.py
+++ tests/test_database_integrity.py
@@ -40,7 +40,7 @@ def test_lottery_code_is_unique_and_required():
     assert column.nullable is False
 
 
-def test_lottery_draw_requires_lottery_and_enforces_unique_number_and_date():
+def test_lottery_draw_requires_lottery_and_enforces_unique_draw_number():
     table = LotteryDraw.__table__
     unique_constraints = {
         tuple(constraint.columns.keys())
@@ -52,7 +52,6 @@ def test_lottery_draw_requires_lottery_and_enforces_unique_number_and_date():
     assert table.c.draw_number.nullable is False
     assert table.c.draw_date.nullable is False
     assert ("lottery_id", "draw_number") in unique_constraints
-    assert ("lottery_id", "draw_date") in unique_constraints
 
 
 def test_lottery_draw_foreign_key_cascades_on_lottery_delete():
