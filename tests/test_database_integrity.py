4343fe3f823262070418d403f40b511d4fff1279
Anonimous <abritovi@gmail.com>
2026-09-21 14:26:15 -0500

    test: update draw uniqueness assertions

--- tests/test_database_integrity.py
+++ tests/test_database_integrity.py
@@ -40,7 +40,7 @@ def test_lottery_code_is_unique_and_required():
     assert column.nullable is False
 
 
-def test_lottery_draw_requires_lottery_and_enforces_unique_number_and_date():
+def test_lottery_draw_requires_lottery_and_enforces_unique_number_and_date_per_draw_type():
     table = LotteryDraw.__table__
     unique_constraints = {
         tuple(constraint.columns.keys())
@@ -49,10 +49,11 @@ def test_lottery_draw_requires_lottery_and_enforces_unique_number_and_date():
     }
 
     assert table.c.lottery_id.nullable is False
+    assert table.c.draw_type.nullable is False
     assert table.c.draw_number.nullable is False
     assert table.c.draw_date.nullable is False
-    assert ("lottery_id", "draw_number") in unique_constraints
-    assert ("lottery_id", "draw_date") in unique_constraints
+    assert ("lottery_id", "draw_type", "draw_number") in unique_constraints
+    assert ("lottery_id", "draw_type", "draw_date") in unique_constraints
 
 
 def test_lottery_draw_foreign_key_cascades_on_lottery_delete():
