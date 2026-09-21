2f9e5610539435405f885f39ed9097ec55cc1903
Anonimous <abritovi@gmail.com>
2026-09-21 06:52:16 -0500

    fix: register ingestion model in metadata

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
+from app.models.ingestion_run import IngestionRun
 from app.models.lottery import Lottery
 from app.models.lottery_draw import LotteryDraw
 from app.models.user import User
 
-__all__ = ["Lottery", "LotteryDraw", "User"]
+__all__ = ["IngestionRun", "Lottery", "LotteryDraw", "User"]
